61c40cb5207d47dfc1de16915fb2e2bd77863cfa
PhillDevTi <philldevti@gmail.com>
2019-01-12 21:36:20 -0200

    Config database

--- Switch.API/Startup.cs
+++ Switch.API/Startup.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Switch.Infra.Data.Context;
 
 namespace Switch.API
 {
@@ -13,8 +12,19 @@ namespace Switch.API
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = Configuration.GetConnectionString("SwitchDB");
+            services.AddDbContext<SwitchContext>(option => option.UseLazyLoadingProxies()
+                                                                 .UseMySQL(conn, m => m.MigrationsAssembly("Switch.Infra.Data")));
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
